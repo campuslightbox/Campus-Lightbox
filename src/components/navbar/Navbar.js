fd9b9a22ac3d7a9023f960519ba8b46c82055168
Sassan Shokoohi <sassan_shokoohi@me.com>
2019-03-08 17:39:57 -0800

    navbar improved

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,25 +1,42 @@
-import React from 'react';
-import { Container, Image, Menu } from 'semantic-ui-react';
+import React, { Component } from 'react'
+import { Input, Menu } from 'semantic-ui-react'
 
-export default () => (
-  <Menu>
-    <Container>
-      <Menu.Item as="a" header>
-        <Image
-          size="small"
-          src="https://www.robinwieruch.de/img/page/logo.svg"
+export default class MenuHeader extends Component {
+  state = { activeItem: 'home' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu color="teal" inverted>
+        <Menu.Item header>CampusLightBox</Menu.Item>
+        <Menu.Item
+          name='home'
+          active={activeItem === 'home'}
+          onClick={this.handleItemClick}
         />
-      </Menu.Item>
-
-      <Menu.Menu position="right">
-        <Menu.Item as="a" name="login">
-          Login
-        </Menu.Item>
-
-        <Menu.Item as="a" name="register">
-          Register
-        </Menu.Item>
-      </Menu.Menu>
-    </Container>
-  </Menu>
-);
+        <Menu.Item name='resources'
+        active={activeItem === 'resources'}
+        onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='aboutUs'
+          active={activeItem === 'aboutUs'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='contactUs'
+          active={activeItem === 'contactUs'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Menu position='right'>
+          <Menu.Item>
+            <Input icon='search' placeholder='Search Resources...' />
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    )
+  }
+}
