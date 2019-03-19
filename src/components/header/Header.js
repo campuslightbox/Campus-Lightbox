35dc363e5334b921af9972dd118218ffe31e6f9b
Shamit Rahman <rahmanshamit@gmail.com>
2019-03-18 22:27:59 -0700

    landing page on Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,38 @@
-import React from 'react'
-import { Header, Icon, Image } from 'semantic-ui-react'
+import React, { Component } from 'react';
+import './css/bootstrap.min.css';
+import './css/etline-font.css';
+import './css/flexslider.css';
+import './css/normalize.min.css';
+import './css/queries.css';
+import './css/styles.css';
 
-const HeaderExampleUsersIcon = () => (
-  <div>
-    <Header as='h2' icon textAlign='center'>
-      <Icon name='hospital' circular />
-      <Header.Content>Campus Light Box</Header.Content>
-    </Header>
-    <Image centered size='large' src='https://react.semantic-ui.com/images/wireframe/centered-paragraph.png' />
-  </div>
-)
 
-export default HeaderExampleUsersIcon
+class Header extends Component {
+    render() {
+        return (
+          <div className = "Header">
+
+          <section className="hero">
+
+              <div className="container">
+                  <div className="row">
+                      <div className="col-md-6 col-md-offset-3">
+                          <div className="hero-content text-center">
+                              <h1>Campus Lightbox</h1>
+                              <p className="intro">an initiative by Project Aurora</p>
+                              <a href="#" className="btn btn-accent btn-large btn-margin-right">Peer</a>
+                              <a href="#" className="btn btn-accent btn-large btn-margin-right">Professional</a>
+                              <a href="#" className="btn btn-accent btn-large ">Phone</a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </section>
+
+
+          </div>
+        );
+    }
+}
+
+export default Header;
