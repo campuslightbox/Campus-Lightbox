8c31daf24df46b1969869d258161357bc5c18e54
Sassan Shokoohi <sassan_shokoohi@me.com>
2019-03-21 18:05:11 -0700

    social media button redone

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,33 +4,27 @@ import {
   Divider,
   Segment,
   Button,
-  Icon
+  Icon,
+  Header
 } from 'semantic-ui-react'
 
 const FixedMenuLayout = () => (
   <div>
-    <Segment color="blue" inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em' }}>
+    <Segment inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em', background: '#40798C'}}>
       <Container textAlign='center'>
-      <div>
-    <Button color='facebook'>
-      <Icon name='facebook' /> Facebook
-    </Button>
-    <Button color='twitter'>
-      <Icon name='twitter' /> Twitter
-    </Button>
-    <Button color='linkedin'>
-      <Icon name='linkedin' /> LinkedIn
-    </Button>
-    <Button color='instagram'>
-      <Icon name='instagram' /> Instagram
-    </Button>
-  </div>
+        <div>
+          <Button  size='massive'circular color='facebook' icon='facebook' />
+          <Button size='massive' circular color='instagram' icon='instagram' />
+          <Button size='massive' circular color='twitter' icon='twitter' />
+          <Button size='massive' circular color='linkedin' icon='linkedin' />
+          
+        </div>
 
         <Divider inverted section />
         <h5>Campus Lightbox</h5>
-        <h6>© Project Aurora | Notice of Non­Affiliation and Disclaimer: We are not affiliated, 
-        associated, authorized, endorsed by, or in any way officially connected with the 
-        the University of British Columbia, or any of its subsidiaries or its affiliates. 
+        <h6>© Project Aurora | Notice of Non­Affiliation and Disclaimer: We are not affiliated,
+        associated, authorized, endorsed by, or in any way officially connected with the
+        the University of British Columbia, or any of its subsidiaries or its affiliates.
         The official University of British Columbia website can be found here.</h6>
       </Container>
     </Segment>
