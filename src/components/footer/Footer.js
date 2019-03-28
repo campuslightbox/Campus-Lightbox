888830af909b710fd678fdba17a6475b74d0cb2b
Sassan Shokoohi <sassan_shokoohi@me.com>
2019-03-28 14:53:31 -0700

    cleaned up footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,29 +1,49 @@
 import React from 'react'
 import {
   Container,
-  Divider,
   Segment,
-  Button
+  Button,
+  Grid,
+  Header
 } from 'semantic-ui-react'
 
 const FixedMenuLayout = () => (
   <div>
-    <Segment inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em', background: '#40798C'}}>
+    <Segment inverted vertical style={{ margin: '5em 0em 0em', padding: '3em 0em', background: '#40798C' }}>
       <Container textAlign='center'>
-        <div>
-          <Button  size='massive'circular color='facebook' icon='facebook' />
-          <Button size='massive' circular color='instagram' icon='instagram' />
-          <Button size='massive' circular color='twitter' icon='twitter' />
-          <Button size='massive' circular color='linkedin' icon='linkedin' />
-          
-        </div>
 
-        <Divider inverted section />
-        <h5>Campus Lightbox</h5>
-        <h6>© Project Aurora | Notice of Non­Affiliation and Disclaimer: We are not affiliated,
+        <Grid stackable>
+
+          <Grid.Column width={5} alignItems="center">
+            <Header inverted as='h4' content='Brought to you by:' />
+            <h5>© Project Aurora</h5>
+            <Button size='large' circular icon='mail' href='mailto:mentalhealthubc@gmail.com' />
+          </Grid.Column>
+
+          <Grid.Column width={4}>
+            <Header inverted as='h4' content='Follow us:' />
+            <div>
+              <a href="https://www.facebook.com/Project-Aurora-1190802371087781/" target="_blank" rel="noopener noreferrer">
+                <Button size='large' circular color='facebook' icon='facebook' /></a>
+              <a href="https://www.instagram.com/proj_aurora/" target="_blank" rel="noopener noreferrer">
+                <Button size='large' circular color='instagram' icon='instagram' /></a>
+              <a href="https://twitter.com/ProjectAurora6" target="_blank" rel="noopener noreferrer">
+                <Button size='large' circular color='twitter' icon='twitter' /></a>
+              <a href="https://www.linkedin.com/company/project-aurora/" target="_blank" rel="noopener noreferrer">
+                <Button size='large' circular color='linkedin' icon='linkedin' /></a>
+            </div>
+          </Grid.Column>
+
+          <Grid.Column width={7}>
+            <Header inverted as='h4' content='Notice of Non­Affiliation and Disclaimer:' />
+            <p>
+              <h6>We are not affiliated,
         associated, authorized, endorsed by, or in any way officially connected with the
         the University of British Columbia, or any of its subsidiaries or its affiliates.
         The official University of British Columbia website can be found here.</h6>
+            </p>
+          </Grid.Column>
+        </Grid>
       </Container>
     </Segment>
   </div>
