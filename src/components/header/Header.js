4f101b36b8dbba65bea49de4c424ee9c0b0240b8
Shamit Rahman <rahmanshamit@gmail.com>
2019-04-04 12:08:40 -0700

    header updated

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Button } from 'semantic-ui-react'
 import GetHelpModal from '../../components/gethelpmodal/GetHelpModal';
-import { Button} from 'semantic-ui-react'
 import './style.css';
 
 
 class Header extends React.Component {
+
+    onItemClick = () => {
+        window.scroll({
+            top: 600,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+
     render() {
         return (
                 <section className = "b1">
-                    <div className="row">
-                        <div className="text-left offset-10">
+                    <div className="helpbutton">
+                        <div className="modal">
                             <GetHelpModal />
                         </div>
                     </div>
                     <div className = "in1" >
-                        <div className = "stuff" >
+                        <div className = "stuff">
                             <h1> Campus Lightbox </h1>
-                        <h2>an initiative by Project Aurora</h2>
+                            <h3>Click on a resource type to get started</h3>
                             <a className="btn" href="#">Peer</a>
                             <a className = "btn" href = "#">Professional</a>
                             <a className="btn" href="#">Phone</a>
+
+                            <div className="ButtonClass">
+                                <Button onClick={this.onItemClick}  inverted
+                                    circular color='orange' icon='angle down' />
+                            <h4>Or scroll down for more options</h4>
+                            </div>
                         </div>
                     </div>
                 </section>
