14e45ad48c8b34bf1e3c7ff91add682d5f7781ba
Shamit Rahman <rahmanshamit@gmail.com>
2019-04-30 21:11:59 -0700

    Updates

--- precache-manifest.ac071a48e8df6d74483e9faf8749a0f0.js
+++ precache-manifest.ac071a48e8df6d74483e9faf8749a0f0.js
@@ -32,12 +32,12 @@ self.__precacheManifest = [
     "url": "/static/media/flags.9c74e172.png"
   },
   {
-    "revision": "cd6c777f1945164224dee082abaea03a",
-    "url": "/static/media/outline-icons.cd6c777f.woff2"
-  },
-  {
     "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
     "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
   },
   {
     "revision": "701ae6abd4719e9c2ada3535a497b341",
@@ -64,12 +64,12 @@ self.__precacheManifest = [
     "url": "/static/media/brand-icons.a046592b.woff"
   },
   {
-    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
-    "url": "/static/media/icons.8e3c7f55.eot"
-  },
-  {
     "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
     "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
   },
   {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
@@ -100,7 +100,7 @@ self.__precacheManifest = [
     "url": "/static/css/2.8916ebf4.chunk.css"
   },
   {
-    "revision": "5bd74e63792ec562d489a46221ad5f5a",
+    "revision": "ffc3d82bb98d2a2d1dbc82f3f27a205f",
     "url": "/index.html"
   }
 ];
