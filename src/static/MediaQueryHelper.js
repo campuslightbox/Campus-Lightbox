b74ecb1790d95c5a35dc3fa60d54ba4d2971aad3
Camellia Peng <camelliapeng02@gmail.com>
2019-06-19 16:18:58 -0700

    Add a file, change constant name for readability

--- src/static/MediaQueryHelper.js
+++ src/static/MediaQueryHelper.js
@@ -1,7 +1,7 @@
 /* More readable versions of queries for MediaQuery component */
 
 export default {
-    MIN_DEVICE_WIDTH_PHONE: "(min-device-width: 320px)",
+    MIN_DEVICE_WIDTH_MOBILE: "(min-device-width: 320px)",
     MIN_DEVICE_WIDTH_TABLET: "(min-device-width: 600px)",
     MIN_DEVICE_WIDTH_LAPTOP: "(min-device-width: 801px)",
 }
