f4cbb086075044676b411aa7e92fd5bccf482147
Camellia Peng <camelliapeng02@gmail.com>
2019-06-19 16:18:21 -0700

    Add react-responsive module, hide filter if on mobile

--- src/containers/mainContainer/MainContainer.js
+++ src/containers/mainContainer/MainContainer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Container, Grid } from 'semantic-ui-react';
+import MediaQuery from 'react-responsive';
+
 import SearchBar from 'components/searchBar/SearchBar';
 import Filter from 'components/filter/Filter';
 import CardContainer from 'containers/cardContainer/CardContainer';
 import Resources from 'static/Resources';
-
-import 'containers/mainContainer/MainContainer.css';
+import MediaQueryHelper from 'static/MediaQueryHelper';
 
 class MainContainer extends React.Component {
     render = () => (
@@ -13,16 +14,18 @@ class MainContainer extends React.Component {
             <div ref={this.props.refProp}></div> 
             <Grid stackable compact="true">
                 <Grid.Column width={4}>
-                    <SearchBar
-                        searchText={this.props.searchText}
-                        onSearchTextChange={this.props.onSearchTextChange}
-                        onClearSearchText={this.props.onClearSearchText}
-                    />
-                    <Filter
-                        filter={this.props.filter}
-                        onFilterChange={this.props.onFilterChange}
-                        onClearFilter={this.props.onClearFilter}
-                    />
+                    <MediaQuery query={MediaQueryHelper.MIN_DEVICE_WIDTH_TABLET}>
+                        <SearchBar
+                            searchText={this.props.searchText}
+                            onSearchTextChange={this.props.onSearchTextChange}
+                            onClearSearchText={this.props.onClearSearchText}
+                        />
+                        <Filter
+                            filter={this.props.filter}
+                            onFilterChange={this.props.onFilterChange}
+                            onClearFilter={this.props.onClearFilter}
+                        />
+                    </MediaQuery>
                 </Grid.Column>
                 <Grid.Column width={12}>
                     <CardContainer
