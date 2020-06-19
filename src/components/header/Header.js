22077ea3931d09af760b8ab51d585c126d008f81
Sassan Shokoohi <sassan_shokoohi@me.com>
2020-06-19 16:48:08 -0700

    imported the quiz app

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react'
+import { Button, Modal, Icon } from 'semantic-ui-react'
 import GetHelpModal from '../../components/gethelpmodal/GetHelpModal';
 import CovidModal from '../../components/covidmodal/CovidModal';
+import QuizApp from '../../components/phase2-quiz/App';
 // import Announcement from '../../components/banner/Announcement';
 import './style.css';
 import '../animate.css';
@@ -26,7 +27,7 @@ class Header extends React.Component {
         ReactGA.event({
             category: 'Header',
             action: 'Clicked Scroll Down Button'
-          });
+        });
         this.props.scrollToContent();
     }
 
@@ -34,15 +35,15 @@ class Header extends React.Component {
         ReactGA.event({
             category: 'Header',
             action: 'Clicked Top Preset Filer: ' + filterName
-          });
+        });
         this.props.onPresetFilterChange(filterName);
         this.props.scrollToContent();
     }
 
     render() {
         return (
-                <section className = "b1">
-                   {/*  {<Announcement
+            <section className="b1">
+                {/*  {<Announcement
                         icon={{name: "wrench"}}
                         text={["Have finance/marketing skills? ", <b>We are hiring!</b>]}
                         button={{
@@ -50,41 +51,52 @@ class Header extends React.Component {
                             onClick: () => window.open("https://docs.google.com/forms/d/e/1FAIpQLSfuV4-Eqm_lB32EjQn1Be_GjRIdETJyavbctmfwvf2fXvpOIw/viewform?usp=sf_link")
                         }}
                     /> } */}
-                    <div className="helpbutton">
-                            <GetHelpModal />
-                    </div>
-                    <div className="covidbutton">
-                            <CovidModal />
-                    </div>
-                        <div className = "stuff">
-                            <img className="titleImage" src={require("./CLB_PrimaryLogo.png")}
-                                      alt = "Campus Lightbox"/>
-                            <div className="guide">
-                            Your Guide to UBC Mental Health Resources
+                <div className="helpbutton">
+                    <GetHelpModal />
+                </div>
+                <div className="covidbutton">
+                    <CovidModal />
+                </div>
+                <div className="stuff">
+                    <img className="titleImage" src={require("./CLB_PrimaryLogo.png")}
+                        alt="Campus Lightbox" />
+                    <div className="guide">
+                        Your Guide to UBC Mental Health Resources
                             </div>
-                            <div className="select">
-                                Select From One of the Following Options
+                    <div className="select">
+                        Select From One of the Following Options
                             </div>
-                            <div className="buttonsAll">
-                            <span className="resourceButton1 animated fadeIn">
+                    <div className="startquiz">
+                        <Modal dimmer="blurring" trigger={<Button color="green"><Icon name='leaf' />Resource Recommender</Button>} closeIcon>
+                            <Modal.Header>Resource Recommender</Modal.Header>
+                            <Modal.Content image>
+                                <Modal.Description>
+                                    <QuizApp />
+                                </Modal.Description>
+                            </Modal.Content>
+                        </Modal>
+                        <br /><br />
+                    </div>
+                    <div className="buttonsAll">
+                        <span className="resourceButton1 animated fadeIn">
                             <Button inverted style={styles.button}
                                 onClick={() => this.onFilterClicked("peer")}>Peer Support</Button>
-                            </span>
-                            <span className="resourceButton2 animated fadeIn">
+                        </span>
+                        <span className="resourceButton2 animated fadeIn">
                             <Button inverted style={styles.button}
                                 onClick={() => this.onFilterClicked("professional")}>Professional Help</Button>
-                            </span>
-                            <span className="resourceButton3 animated fadeIn">
+                        </span>
+                        <span className="resourceButton3 animated fadeIn">
                             <Button inverted style={styles.button}
                                 onClick={() => this.onFilterClicked("hotline")}>Phone Hotline</Button>
-                            </span>
-                            </div>
-                            <div className="ButtonClass animated fadeInDown">
-                                <Button onClick={this.onScrollDownClicked} inverted
-                                    circular icon='angle down' />
-                            </div>
-                        </div>
-                </section>
+                        </span>
+                    </div>
+                    <div className="ButtonClass animated fadeInDown">
+                        <Button onClick={this.onScrollDownClicked} inverted
+                            circular icon='angle down' />
+                    </div>
+                </div>
+            </section>
         )
     }
 }
