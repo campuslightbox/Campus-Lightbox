1caf9095f6edde18c4a01dcbb5b05e8f295ece25
Sassan Shokoohi <sassan_shokoohi@me.com>
2021-06-26 15:34:13 -0700

    👌 IMPROVE: removed duplicate CrisisResponse. Header & GetHelpModal converted to functional components

--- src/components/getHelpModal/GetHelpModal.js
+++ src/components/getHelpModal/GetHelpModal.js
@@ -1,69 +1,55 @@
-import { Button, Header, Icon, Modal, Table } from "semantic-ui-react";
-import React, { Component } from "react";
+import { Header, Icon, Modal, Table } from "semantic-ui-react";
 
-import ReactGA from "react-ga";
+function GetHelpModal({ isCrisis, setIsCrisis }) {
+  // initial state is open so that it will appear immediately if selections match crisis response
 
-ReactGA.initialize("UA-139413334-1");
+  return (
+    <Modal
+      dimmer="blurring"
+      open={isCrisis}
+      onClose={() => setIsCrisis(false)}
+      closeIcon
+    >
+      <Modal.Header>Get Help Now </Modal.Header>
+      <Modal.Content image>
+        <div className="image">
+          <Icon name="right arrow" />
+        </div>
+        <Modal.Description>
+          <Header>Help is Available!</Header>
+          <p>
+            If you or someone else is in immediate danger, or at risk of harming
+            yourself or others <strong>CALL 911</strong> and state{" "}
+            <strong>"UBC Dispatch"</strong>.
+          </p>
 
-export default class MenuHeader extends Component {
-  trackGA = (buttonName) => {
-    ReactGA.event({
-      category: "Header",
-      action: "Clicked " + buttonName + " Button",
-    });
-  };
-
-  render() {
-    return (
-      <Modal
-        dimmer="blurring"
-        trigger={
-          <Button onClick={() => this.trackGA("Get Help Now")} color="red">
-            <Icon name="heart outline" />
-            Get Help Now
-          </Button>
-        }
-        closeIcon
-      >
-        <Modal.Header>Get Help Now</Modal.Header>
-        <Modal.Content image>
-          <div className="image">
-            <Icon name="right arrow" />
-          </div>
-          <Modal.Description>
-            <Header>Help is Available!</Header>
-            <p>
-              If you or someone else is in immediate danger, or at risk of
-              harming yourself or others <strong>CALL 911</strong> and state{" "}
-              <strong>"UBC Dispatch"</strong>.
-            </p>
-
-            <Table definition>
-              <Table.Body>
-                <Table.Row>
-                  <Table.Cell>
-                    Crisis Center Line 24/7 <Icon name="phone" /> (604-872-3311)
-                    BC-Wide: (1-800-784-2433)
-                  </Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                  <Table.Cell>
-                    Woman Against Violence Against Women 24/7{" "}
-                    <Icon name="phone" />
-                    (604-255-6344)
-                  </Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                  <Table.Cell>
-                    UBC Campus Security 24/7 <Icon name="phone" />
-                    (604-822-2222)
-                  </Table.Cell>
-                </Table.Row>
-              </Table.Body>
-            </Table>
-          </Modal.Description>
-        </Modal.Content>
-      </Modal>
-    );
-  }
+          <Table definition>
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell>
+                  Crisis Center Line 24/7 <Icon name="phone" /> (604-872-3311)
+                  BC-Wide: (1-800-784-2433)
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>
+                  Woman Against Violence Against Women 24/7{" "}
+                  <Icon name="phone" />
+                  (604-255-6344)
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>
+                  UBC Campus Security 24/7 <Icon name="phone" />
+                  (604-822-2222)
+                </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table>
+        </Modal.Description>
+      </Modal.Content>
+    </Modal>
+  );
 }
+
+export default GetHelpModal;
